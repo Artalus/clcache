d94dd81327485675597bb1f49fd9a2d8d1053f27
Artalus <artalus-mail@yandex.ru>
2021-09-25 13:21:20 +0300

    chore: split off CacheLock

--- clcache/clcache.py
+++ clcache/clcache.py
@@ -29,6 +29,7 @@ from atomicwrites import atomic_write
 from .errors import *
 from .print import *
 from .cmdline import CommandLineAnalyzer
+from .lock import CacheLock
 
 VERSION = "4.2.0-dev"
 
@@ -263,62 +264,6 @@ class ManifestRepository:
     @staticmethod
     def getIncludesContentHashForHashes(listOfHashes):
         return HashAlgorithm(','.join(listOfHashes).encode()).hexdigest()
-
-
-class CacheLock:
-    """ Implements a lock for the object cache which
-    can be used in 'with' statements. """
-    INFINITE = 0xFFFFFFFF
-    WAIT_ABANDONED_CODE = 0x00000080
-    WAIT_TIMEOUT_CODE = 0x00000102
-
-    def __init__(self, mutexName, timeoutMs):
-        self._mutexName = 'Local\\' + mutexName
-        self._mutex = None
-        self._timeoutMs = timeoutMs
-
-    def createMutex(self):
-        self._mutex = windll.kernel32.CreateMutexW(
-            None,
-            wintypes.BOOL(False),
-            self._mutexName)
-        assert self._mutex
-
-    def __enter__(self):
-        self.acquire()
-
-    def __exit__(self, typ, value, traceback):
-        self.release()
-
-    def __del__(self):
-        if self._mutex:
-            windll.kernel32.CloseHandle(self._mutex)
-
-    def acquire(self):
-        if not self._mutex:
-            self.createMutex()
-        result = windll.kernel32.WaitForSingleObject(
-            self._mutex, wintypes.INT(self._timeoutMs))
-        if result not in [0, self.WAIT_ABANDONED_CODE]:
-            if result == self.WAIT_TIMEOUT_CODE:
-                errorString = \
-                    'Failed to acquire lock {} after {}ms; ' \
-                    'try setting CLCACHE_OBJECT_CACHE_TIMEOUT_MS environment variable to a larger value.'.format(
-                        self._mutexName, self._timeoutMs)
-            else:
-                errorString = 'Error! WaitForSingleObject returns {result}, last error {error}'.format(
-                    result=result,
-                    error=windll.kernel32.GetLastError())
-            raise CacheLockException(errorString)
-
-    def release(self):
-        windll.kernel32.ReleaseMutex(self._mutex)
-
-    @staticmethod
-    def forPath(path):
-        timeoutMs = int(os.environ.get('CLCACHE_OBJECT_CACHE_TIMEOUT_MS', 10 * 1000))
-        lockName = path.replace(':', '-').replace('\\', '-')
-        return CacheLock(lockName, timeoutMs)
 
 
 class CompilerArtifactsSection:
