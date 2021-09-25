d87f3af71778557e61de7b0fddd3d7eb33a7162b
Artalus <artalus-mail@yandex.ru>
2021-09-25 15:36:21 +0300

    chore: split off Manifest

--- clcache/clcache.py
+++ clcache/clcache.py
@@ -91,24 +91,6 @@ def setCachedCompilerConsoleOutput(path, output):
     with open(path, 'wb') as f:
         f.write(output.encode(CACHE_COMPILER_OUTPUT_STORAGE_CODEC))
 
-class Manifest:
-    def __init__(self, entries=None):
-        if entries is None:
-            entries = []
-        self._entries = entries.copy()
-
-    def entries(self):
-        return self._entries
-
-    def addEntry(self, entry):
-        """Adds entry at the top of the entries"""
-        self._entries.insert(0, entry)
-
-    def touchEntry(self, objectHash):
-        """Moves entry in entryIndex position to the top of entries()"""
-        entryIndex = next((i for i, e in enumerate(self.entries()) if e.objectHash == objectHash), 0)
-        self._entries.insert(0, self._entries.pop(entryIndex))
-
 
 class ManifestSection:
     def __init__(self, manifestSectionDir):
--- clcache/manifest.py
+++ clcache/manifest.py
@@ -8,3 +8,21 @@ from collections import namedtuple
 # `objectHash`: hash of the object in cache
 ManifestEntry = namedtuple('ManifestEntry', ['includeFiles', 'includesContentHash', 'objectHash'])
 
+
+class Manifest:
+    def __init__(self, entries=None):
+        if entries is None:
+            entries = []
+        self._entries = entries.copy()
+
+    def entries(self):
+        return self._entries
+
+    def addEntry(self, entry):
+        """Adds entry at the top of the entries"""
+        self._entries.insert(0, entry)
+
+    def touchEntry(self, objectHash):
+        """Moves entry in entryIndex position to the top of entries()"""
+        entryIndex = next((i for i, e in enumerate(self.entries()) if e.objectHash == objectHash), 0)
+        self._entries.insert(0, self._entries.pop(entryIndex))
