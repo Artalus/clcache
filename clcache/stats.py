1fc9b223e390cfd1b3348769a0835549fd580882
Artalus <artalus-mail@yandex.ru>
2021-09-28 22:30:01 +0300

    chore: type hints for stats.py

--- clcache/stats.py
+++ clcache/stats.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any, Optional, cast
 
 from atomicwrites import atomic_write
 
@@ -7,7 +8,7 @@ from .print import printErrStr
 
 
 class PersistentJSONDict:
-    def __init__(self, fileName):
+    def __init__(self, fileName: str):
         self._dirty = False
         self._dict = {}
         self._fileName = fileName
@@ -19,22 +20,22 @@ class PersistentJSONDict:
         except ValueError:
             printErrStr("clcache: persistent json file %s was broken" % fileName)
 
-    def save(self):
+    def save(self) -> None:
         if self._dirty:
             with atomic_write(self._fileName, overwrite=True) as f:
                 json.dump(self._dict, f, sort_keys=True, indent=4)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Any) -> None:
         self._dict[key] = value
         self._dirty = True
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> Any:
         return self._dict[key]
 
-    def __contains__(self, key):
+    def __contains__(self, key: str) -> bool:
         return key in self._dict
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         return type(self) is type(other) and self.__dict__ == other.__dict__
 
 
@@ -73,120 +74,153 @@ class Statistics:
         CACHE_SIZE,
     }
 
-    def __init__(self, statsFile):
+    _stats: Optional[PersistentJSONDict]
+    def __init__(self, statsFile: str):
         self._statsFile = statsFile
         self._stats = None
         self.lock = CacheLock.forPath(self._statsFile)
 
-    def __enter__(self):
+    def __enter__(self) -> "Statistics":
         self._stats = PersistentJSONDict(self._statsFile)
         for k in Statistics.RESETTABLE_KEYS | Statistics.NON_RESETTABLE_KEYS:
             if k not in self._stats:
                 self._stats[k] = 0
         return self
 
-    def __exit__(self, typ, value, traceback):
+    def __exit__(self, typ: Any, value: Any, traceback: Any) -> None:
         # Does not write to disc when unchanged
+        assert self._stats
         self._stats.save()
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         return type(self) is type(other) and self.__dict__ == other.__dict__
 
-    def numCallsWithInvalidArgument(self):
-        return self._stats[Statistics.CALLS_WITH_INVALID_ARGUMENT]
+    def numCallsWithInvalidArgument(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CALLS_WITH_INVALID_ARGUMENT])
 
-    def registerCallWithInvalidArgument(self):
+    def registerCallWithInvalidArgument(self) -> None:
+        assert self._stats
         self._stats[Statistics.CALLS_WITH_INVALID_ARGUMENT] += 1
 
-    def numCallsWithoutSourceFile(self):
-        return self._stats[Statistics.CALLS_WITHOUT_SOURCE_FILE]
+    def numCallsWithoutSourceFile(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CALLS_WITHOUT_SOURCE_FILE])
 
-    def registerCallWithoutSourceFile(self):
+    def registerCallWithoutSourceFile(self) -> None:
+        assert self._stats
         self._stats[Statistics.CALLS_WITHOUT_SOURCE_FILE] += 1
 
-    def numCallsWithMultipleSourceFiles(self):
-        return self._stats[Statistics.CALLS_WITH_MULTIPLE_SOURCE_FILES]
+    def numCallsWithMultipleSourceFiles(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CALLS_WITH_MULTIPLE_SOURCE_FILES])
 
-    def registerCallWithMultipleSourceFiles(self):
+    def registerCallWithMultipleSourceFiles(self) -> None:
+        assert self._stats
         self._stats[Statistics.CALLS_WITH_MULTIPLE_SOURCE_FILES] += 1
 
-    def numCallsWithPch(self):
-        return self._stats[Statistics.CALLS_WITH_PCH]
+    def numCallsWithPch(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CALLS_WITH_PCH])
 
-    def registerCallWithPch(self):
+    def registerCallWithPch(self) -> None:
+        assert self._stats
         self._stats[Statistics.CALLS_WITH_PCH] += 1
 
-    def numCallsForLinking(self):
-        return self._stats[Statistics.CALLS_FOR_LINKING]
+    def numCallsForLinking(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CALLS_FOR_LINKING])
 
-    def registerCallForLinking(self):
+    def registerCallForLinking(self) -> None:
+        assert self._stats
         self._stats[Statistics.CALLS_FOR_LINKING] += 1
 
-    def numCallsForExternalDebugInfo(self):
-        return self._stats[Statistics.CALLS_FOR_EXTERNAL_DEBUG_INFO]
+    def numCallsForExternalDebugInfo(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CALLS_FOR_EXTERNAL_DEBUG_INFO])
 
-    def registerCallForExternalDebugInfo(self):
+    def registerCallForExternalDebugInfo(self) -> None:
+        assert self._stats
         self._stats[Statistics.CALLS_FOR_EXTERNAL_DEBUG_INFO] += 1
 
-    def numEvictedMisses(self):
-        return self._stats[Statistics.EVICTED_MISSES]
+    def numEvictedMisses(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.EVICTED_MISSES])
 
-    def registerEvictedMiss(self):
+    def registerEvictedMiss(self) -> None:
+        assert self._stats
         self.registerCacheMiss()
         self._stats[Statistics.EVICTED_MISSES] += 1
 
-    def numHeaderChangedMisses(self):
-        return self._stats[Statistics.HEADER_CHANGED_MISSES]
+    def numHeaderChangedMisses(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.HEADER_CHANGED_MISSES])
 
-    def registerHeaderChangedMiss(self):
+    def registerHeaderChangedMiss(self) -> None:
+        assert self._stats
         self.registerCacheMiss()
         self._stats[Statistics.HEADER_CHANGED_MISSES] += 1
 
-    def numSourceChangedMisses(self):
-        return self._stats[Statistics.SOURCE_CHANGED_MISSES]
+    def numSourceChangedMisses(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.SOURCE_CHANGED_MISSES])
 
-    def registerSourceChangedMiss(self):
+    def registerSourceChangedMiss(self) -> None:
+        assert self._stats
         self.registerCacheMiss()
         self._stats[Statistics.SOURCE_CHANGED_MISSES] += 1
 
-    def numCacheEntries(self):
-        return self._stats[Statistics.CACHE_ENTRIES]
+    def numCacheEntries(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CACHE_ENTRIES])
 
-    def setNumCacheEntries(self, number):
+    def setNumCacheEntries(self, number: int) -> None:
+        assert self._stats
         self._stats[Statistics.CACHE_ENTRIES] = number
 
-    def registerCacheEntry(self, size):
+    def registerCacheEntry(self, size: int) -> None:
+        assert self._stats
         self._stats[Statistics.CACHE_ENTRIES] += 1
         self._stats[Statistics.CACHE_SIZE] += size
 
-    def unregisterCacheEntry(self, size):
+    def unregisterCacheEntry(self, size: int) -> None:
+        assert self._stats
         self._stats[Statistics.CACHE_ENTRIES] -= 1
         self._stats[Statistics.CACHE_SIZE] -= size
 
-    def currentCacheSize(self):
-        return self._stats[Statistics.CACHE_SIZE]
+    def currentCacheSize(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CACHE_SIZE])
 
-    def setCacheSize(self, size):
+    def setCacheSize(self, size: int) -> None:
+        assert self._stats
         self._stats[Statistics.CACHE_SIZE] = size
 
-    def numCacheHits(self):
-        return self._stats[Statistics.CACHE_HITS]
+    def numCacheHits(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CACHE_HITS])
 
-    def registerCacheHit(self):
+    def registerCacheHit(self) -> None:
+        assert self._stats
         self._stats[Statistics.CACHE_HITS] += 1
 
-    def numCacheMisses(self):
-        return self._stats[Statistics.CACHE_MISSES]
+    def numCacheMisses(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CACHE_MISSES])
 
-    def registerCacheMiss(self):
+    def registerCacheMiss(self) -> None:
+        assert self._stats
         self._stats[Statistics.CACHE_MISSES] += 1
 
-    def numCallsForPreprocessing(self):
-        return self._stats[Statistics.CALLS_FOR_PREPROCESSING]
+    def numCallsForPreprocessing(self) -> int:
+        assert self._stats
+        return cast(int, self._stats[Statistics.CALLS_FOR_PREPROCESSING])
 
-    def registerCallForPreprocessing(self):
+    def registerCallForPreprocessing(self) -> None:
+        assert self._stats
         self._stats[Statistics.CALLS_FOR_PREPROCESSING] += 1
 
-    def resetCounters(self):
+    def resetCounters(self) -> None:
+        assert self._stats
         for k in Statistics.RESETTABLE_KEYS:
             self._stats[k] = 0
