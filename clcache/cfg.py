086fd4ff6637f7bd985a35568982b45958e65aa8
Artalus <artalus-mail@yandex.ru>
2021-09-28 22:16:25 +0300

    chore: type hints for cfg.py

--- clcache/cfg.py
+++ clcache/cfg.py
@@ -1,26 +1,32 @@
+from typing import Any, Dict
+
 from .stats import PersistentJSONDict
 
 
 class Configuration:
-    _defaultValues = {"MaximumCacheSize": 1073741824} # 1 GiB
+    _defaultValues: Dict[str, Any] = {"MaximumCacheSize": 1073741824} # 1 GiB
 
-    def __init__(self, configurationFile):
+    def __init__(self, configurationFile: str):
         self._configurationFile = configurationFile
         self._cfg = None
 
-    def __enter__(self):
+    # TODO: return separate ConfigurationLocked class
+    def __enter__(self) -> "Configuration":
         self._cfg = PersistentJSONDict(self._configurationFile)
         for setting, defaultValue in self._defaultValues.items():
             if setting not in self._cfg:
                 self._cfg[setting] = defaultValue
         return self
 
-    def __exit__(self, typ, value, traceback):
+    def __exit__(self, typ: Any, value: Any, traceback: Any) -> None:
         # Does not write to disc when unchanged
+        assert self._cfg
         self._cfg.save()
 
-    def maximumCacheSize(self):
+    def maximumCacheSize(self) -> int:
+        assert self._cfg
         return self._cfg["MaximumCacheSize"]
 
-    def setMaximumCacheSize(self, size):
+    def setMaximumCacheSize(self, size: int) -> None:
+        assert self._cfg
         self._cfg["MaximumCacheSize"] = size
